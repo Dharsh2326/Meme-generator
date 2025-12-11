31c0524ccb6417272d4f156e10017112b7f3bd0b
Dharshini Muthukrishnan <dharsh.3026@gmail.com>
2025-12-11 12:33:27 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx"
+import Main from "./components/Main"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
+
+
